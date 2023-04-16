2e56d6d038d5ea2425e09d7ab9ea34eb13e251d8
Sanket Singh <singhsanket143@gmail.com>
2023-04-16 13:10:09 +0530

    Added binary search

--- algorithms/index.js
+++ algorithms/index.js
@@ -1,5 +1,6 @@
 module.exports = {
     BubbleSort: require('./bubblesort'),
     MergeSort: require('./mergesort'),
-    QuickSort: require('./quicksort')
+    QuickSort: require('./quicksort'),
+    BinarySearch: require('./binarysearch'),
 }
